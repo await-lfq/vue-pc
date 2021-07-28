5ab13c724a6ba810bd6fa64ef9763f3040d6273b
await-lfq <15797800251@139.com>
2021-07-28 23:04:35 +0800

    懒加载优化

--- src/views/lazyLoading.vue
+++ src/views/lazyLoading.vue
@@ -103,33 +103,39 @@
 export default {
   data() {
     return {
-      // 浏览器可视区域的高度
-      innerHeight: window.innerHeight,
+      // 指示器，表示图片显示的数量
+      cursor: 0,
+      // 定时器
+      timer: "",
     };
   },
   mounted() {
+    let oImgs = document.querySelectorAll(".img-box");
+    let innerHeight=window.innerHeight;
     // 初始化页面时
-    this.lazy(this.innerHeight, document.querySelectorAll(".img-box"));
+    this.lazy(innerHeight, oImgs, oImgs.length);
     // 监听浏览器滚动事件
     window.onscroll = () => {
-      this.lazy(this.innerHeight, document.querySelectorAll(".img-box"));
+      if (this.cursor === length) return;
+      if (this.timer) clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        this.lazy(innerHeight, oImgs, oImgs.length);
+      }, 300);
     };
   },
   methods: {
     /**
-     * @description 懒加载图片，需给盒子设置一个高度
+     * @description 懒加载图片
      * @method lazy
      * @params {Number,Array对象}  浏览器可视区域的高度 图片数组
      */
-    lazy(h, imgs) {
+    lazy(h, imgs, length) {
       let s = document.documentElement.scrollTop; // 滚动的距离
-      for (let i = 0, len = imgs.length; i < len; i++) {
+      for (let i = this.cursor; i < length; i++) {
         // 图片出现在可视区域时，显示图片
         if (h + s > imgs[i].offsetTop) {
-          setTimeout(() => {
-            // 图片过一段时间再显示
-            imgs[i].children[0].src = imgs[i].children[0].dataset.src;
-          }, 1000);
+          this.cursor++;
+          imgs[i].children[0].src = imgs[i].children[0].dataset.src;
         }
       }
     },
